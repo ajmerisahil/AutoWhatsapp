6409226bdd03eb042023e337c3200e1d6221a96c
Sahil Ajmeri <sahil.ms1113@gmail.com>
2021-03-03 12:20:20 +0530

    Initial commit

---
+++ index.py
@@ -0,0 +1,4 @@
+import pywhatkit
+
+pywhatkit.sendwhatmsg("+918530401613", "Hello from Sahil ! This message is automated" , 12 , 17)
+pywhatkit.sendwhatmsg("+919714292326", "Hello from Sahil ! This message is automated" , 12 , 18)
